081e0ccbadba87dcfb22512cc78eff115e9ee43e
Ana Ventura <anaventura1811@gmail.com>
2024-10-26 16:22:50 -0300

    feat: ajusta requirements.txt para incluir outras dependências, ajustes nos imports do app, cria model de user

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
+
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
